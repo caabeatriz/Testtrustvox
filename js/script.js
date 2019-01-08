aa4205d5d35a7f0a4d46bc6eeb5b5edfd06c0ffb
Caroline Silva <contatocarolinebs@gmail.com>
2019-01-08 10:53:31 -0200

    Modified function calcular inputs

--- js/script.js
+++ js/script.js
@@ -31,13 +31,30 @@ btnAdd.addEventListener("click", function(event){
     tableTr.appendChild(selectTd)
     tableTr.appendChild(dateTd)
 
-    const table = document.querySelector("#table-activits")
-    table.appendChild(tableTr)
-    
-    
+   
+    // cria uma div para colocar o botao delete e insere ele com inner 
+    const btnDeleteTask = document.createElement("div")
+    btnDeleteTask.className = "btnDeleteTask"
+    btnDeleteTask.innerHTML = ` - `
 
+    //Botao excluir tarefas 
+    btnDeleteTask.addEventListener("click", function (eventdelete) {
+        eventdelete.preventDefault()
+        tableTr.remove()
+    })
     
     
+    tableTr.appendChild(btnDeleteTask)
+    const table = document.querySelector("#table-activits")
+    table.appendChild(tableTr)  
+
+    function Calcular(){
+        var resultado = document.querySelector(".resultado")
+
+        
+    }
 
     
 })
+
+
